bad1405935bd1b335faab94fd89416112b2405bc
Git_test <rimmoyee@gmail.com>
2019-07-27 19:32:30 +0900

    비행체 표면 회전 중간 업데이트 2
    비행체 표면 업데이트 내역
    Aileron L, R
    Elevator L, R

--- Assets/02.Scripts/JH/AviationControl/F16SureFaceControl.cs
+++ Assets/02.Scripts/JH/AviationControl/F16SureFaceControl.cs
@@ -32,11 +32,13 @@ public class F16SureFaceControl : MonoBehaviour
 
     //사용 수학함수
     public float Epsilon;
+    public float PitchRollCheck;
 
     // Start is called before the first frame update
     void Start()
     {
         Epsilon = Mathf.Epsilon + 1 - 1;    //근사값 0으로 제대로 맞춰주기 위해 1 더하고 1 뺌
+        PitchRollCheck = 0;                 //pitch값과 roll값을 확인해서 보간줄 조건확인위한 변수
 
         VoletRange = Epsilon + 10;
         AileronRange = Epsilon + 25;
@@ -48,16 +50,20 @@ public class F16SureFaceControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pitch = aviationManager.pitch;      //pitch값 가져옴    -1 ~ 1
+        pitch = -aviationManager.pitch;      //pitch값 가져옴    -1 ~ 1
         yaw = aviationManager.yaw;          //yaw값 가져옴      -1 ~ 1
         roll = aviationManager.roll;        //roll값 가져옴     -1 ~ 1
-
+        
         VoletL.transform.localRotation = Quaternion.Euler(new Vector3(pitch * VoletRange, -34.747f, -1.253f));
         VoletR.transform.localRotation = Quaternion.Euler(new Vector3(pitch * VoletRange, 34.747f, 1.253f));
 
+        AileronL.transform.localRotation = Quaternion.Euler(new Vector3((pitch * 2 + roll * 1.2f)/2 * AileronRange, -8.91f, -1.395f));
+        AileronR.transform.localRotation = Quaternion.Euler(new Vector3((pitch * 2 + -roll * 1.2f)/2 * AileronRange, 8.91f, 1.395f));
+
+        ElevatorL.transform.localRotation = Quaternion.Euler(new Vector3(-(pitch * 2 + roll * 1.2f)/2 * ElevatorRange, 0, 0));
+        ElevatorR.transform.localRotation = Quaternion.Euler(new Vector3(-(pitch * 2 + -roll * 1.2f)/2 * ElevatorRange, 0, 0));
+
         Rudder.transform.localRotation = Quaternion.Euler(new Vector3(0, yaw * RudderRange, 0));
-        //print(aviationManager.controlAngle.x);
-        //print(aviationManager.controlAngle.y);
-        //print(aviationManager.controlAngle.z);
+        print(pitch * 2 + roll * 1.2f);
     }
 }
--- Assets/02.Scripts/JH/ControllerInputCheck.cs
+++ Assets/02.Scripts/JH/ControllerInputCheck.cs
@@ -6,9 +6,10 @@ using Valve.VR;
 public class ControllerInputCheck : MonoBehaviour
 {
 	public SteamVR_Input_Sources rightController;
-	public SteamVR_Action_Boolean isTriggerClicked;     //트리거 클릭 확인
-	public SteamVR_Action_Vector2 isTrackpadDragged;    //트랙패드 드래깅 확인
-	public SteamVR_Action_Pose poseTracker;
+
+	public SteamVR_Action_Boolean RTriggerClicked;     //트리거 클릭 확인
+	public SteamVR_Action_Vector2 RTrackpadDragged;    //트랙패드 드래깅 확인
+	public SteamVR_Action_Pose RPoseTracker;
 
 	[HideInInspector] public bool booster = false;
 	[HideInInspector] public float throttle = 0;
@@ -20,38 +21,26 @@ public class ControllerInputCheck : MonoBehaviour
     void Update()
     {
 		//컨트롤러 각도 갱신
-		controllerAngle = poseTracker.localRotation;
+		controllerAngle = RPoseTracker.localRotation;
 		
 		//트리거 눌렀을 때(한번)
-		if(isTriggerClicked.stateDown)
+		if(RTriggerClicked.stateDown)
 		{
-			booster = true;             //print(booster);
-			//testMgr.LFlame.material.Lerp(testMgr.BReady, testMgr.RReady, 1f);
+			booster = true;
 
 		}
 		//트리거 뗐을 때(한번)
-		else if(isTriggerClicked.stateUp)
+		else if(RTriggerClicked.stateUp)
 		{
-			booster = false;            //print(booster);
-			//testMgr.LFlame.material.Lerp(testMgr.RReady, testMgr.BReady, 1f);
+			booster = false;
 		}
 
 		//트랙패드 값 변경시
-		if(isTrackpadDragged.changed)
+		if(RTrackpadDragged.changed)
 		{
 			throttle = Mathf.Lerp(throttle
-								  , ((isTrackpadDragged.axis.y + 1f) / 2)
+								  , ((RTrackpadDragged.axis.y + 1f) / 2)
 								  , 0.1f);
 		}
-
-
-
-		//print(poseTracker.angularVelocity);		//각가속도
-		//print(poseTracker.changed);				//벡터변화 확인
-		//print(poseTracker.velocity);				//컨트롤러 이동속도
-
-		//float pitch = Mathf.Clamp(poseTracker.localRotation.x, Mathf.Epsilon - 0.8f, Mathf.Epsilon);
-		//float yaw = Mathf.Clamp(poseTracker.localRotation.y, Mathf.Epsilon - 0.7f, Mathf.Epsilon + 0.7f);
-		//float roll = Mathf.Clamp(poseTracker.localRotation.z, Mathf.Epsilon - 0.7f, Mathf.Epsilon + 0.7f);
 	}
 }
