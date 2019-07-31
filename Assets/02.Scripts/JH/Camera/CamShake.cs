50f4ada746344cc009b352a4ccd60cd7e9febfb8
Git_test <rimmoyee@gmail.com>
2019-07-31 09:32:28 +0900

    카메라 진동 코드 확인
    카메라 진동 코드를 확인하기 위한 팹

---
+++ Assets/02.Scripts/JH/Camera/CamShake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+    //카메라 흔들기
+    public float ShakeAmount;
+    float ShakeTime;
+    Vector3 initialPosition;
+
+    public void VibrateForTime(float time)
+    {
+        ShakeTime = time;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShakeTime = 5f;
+        initialPosition = new Vector3(0, 0, -5f);
+        UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ShakeTime > 0)
+        {
+            transform.position = Random.insideUnitSphere * ShakeAmount + initialPosition;
+            ShakeTime -= Time.deltaTime;
+        }
+        else
+        {
+            ShakeTime = 0.0f;
+            transform.position = initialPosition;
+        }
+    }
+}
