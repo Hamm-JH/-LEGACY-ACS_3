2dc0fc124355bf64c985a97dddef5ac15634826d
Git_test <rimmoyee@gmail.com>
2019-07-31 22:05:34 +0900

    HUD 초기버전 구현

---
+++ Assets/02.Scripts/JH/HUDUI/FrontGlassHUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FrontGlassHUD : MonoBehaviour
+{
+    public Transform camera;
+
+    public Transform horizontalLine;
+    public Transform VerticalLine;
+
+    public float pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(camera.localRotation.x > -0.7f && camera.localRotation.x < 0.7f)
+        {
+            pitch = camera.localRotation.x / 7 * 10;
+        }
+        print(pitch * 900);
+        VerticalLine.localPosition = new Vector3(0, pitch * 900, 0);
+    }
+}
