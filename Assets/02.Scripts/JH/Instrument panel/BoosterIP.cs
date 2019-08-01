efa722c08426777d97427c1083fbb237868a52e6
Git_test <rimmoyee@gmail.com>
2019-08-01 21:37:07 +0900

    속도표시기, 부스터계 생성
    속도표시기, 부스터계 생성

---
+++ Assets/02.Scripts/JH/Instrument panel/BoosterIP.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterIP : MonoBehaviour
+{
+    public AviationManager aviationManager;
+
+    public Transform scaleArrow;
+
+    private Vector3 idleState;
+    private Vector3 upState;
+
+    private int boostState;
+
+    private float upTime;
+    private float downTime;
+
+    private float currRoll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        idleState = new Vector3(0, 0, 57);
+        upState = new Vector3(0, 0, -115);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boostState = aviationManager.booster;
+        //print(boostState);
+        currRoll = scaleArrow.transform.localRotation.eulerAngles.z;
+        //print(currRoll);
+
+        if(boostState == 1)
+        {
+            downTime = 0;
+
+            scaleArrow.localRotation = Quaternion.Slerp(scaleArrow.transform.localRotation
+                                                    , Quaternion.Euler(upState)
+                                                    , upTime);
+
+            upTime += Time.deltaTime;
+        }
+        else if(boostState == 0)
+        {
+            upTime = 0;
+
+            scaleArrow.localRotation = Quaternion.Slerp(scaleArrow.transform.localRotation
+                                                    , Quaternion.Euler(idleState)
+                                                    , downTime);
+
+            downTime += Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/02.Scripts/JH/Instrument panel/ThrottleIP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrottleIP : MonoBehaviour
+{
+    public AviationManager aviationManager;
+
+    public Transform scaleArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleArrow.localRotation = Quaternion.Euler( new Vector3(0, 0, 180 - aviationManager.throttle * 295));
+    }
+}
