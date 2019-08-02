f8bb4efc3178a1698df59600762488e40d00214c
Git_test <rimmoyee@gmail.com>
2019-08-02 10:48:05 +0900

    PFD AirSpeedIndicator 구현
    Primary Flight Display
    AirSpeedindicator 구현완료

---
+++ Assets/02.Scripts/JH/Instrument panel/PFD/AirSpeedIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirSpeedIndicator : MonoBehaviour
+{
+    public PFDManager PFD;
+
+    public Transform speedIndicator;
+
+    private float _currForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //속도 입력값
+        _currForce = PFD.aviMeter.thrustControl.currForce / 4;
+        
+        //속도계 위치값 변경
+        speedIndicator.localPosition = new Vector3(0, 700 - (_currForce / 2125 * 1257), 0);
+    }
+}
