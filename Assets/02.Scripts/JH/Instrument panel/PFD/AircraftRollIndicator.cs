0a0edd41259759f24dfc1140028bad45117616f0
Git_test <rimmoyee@gmail.com>
2019-08-02 14:29:11 +0900

    AircraftRollIndicator 구현

---
+++ Assets/02.Scripts/JH/Instrument panel/PFD/AircraftRollIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AircraftRollIndicator : MonoBehaviour
+{
+    public Transform fighter;
+
+    public Transform aircraftRollIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aircraftRollIndicator.localRotation = Quaternion.Euler(new Vector3(0, 0, fighter.rotation.z));
+            //fighter.rotation;
+    }
+}
