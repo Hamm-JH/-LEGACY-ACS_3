9759e23a828f5c9fa2930b8699bf2b674ff5c507
Git_test <rimmoyee@gmail.com>
2019-08-02 20:43:40 +0900

    좌우측 HDD 구현

---
+++ Assets/02.Scripts/JH/Instrument panel/PFD/AircraftPitchIndicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AircraftPitchIndicator : MonoBehaviour
+{
+    public AviationManager aviationManager;
+
+    public Transform pitchIndicator;
+
+    private float pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(aviationManager._controllerAngle.x >= -0.8f && aviationManager._controllerAngle.x <= 0)
+        {
+            pitch = (aviationManager._controllerAngle.x + 0.4f) / 4 * 10;
+        }
+
+
+        pitchIndicator.localPosition = new Vector3(0, -11.5f + pitch * 89.8f * 3, 0);
+        print(pitchIndicator.localPosition.y);
+    }
+}
