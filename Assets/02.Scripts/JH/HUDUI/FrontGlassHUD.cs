15a06a3ea25f45ca3914f20cc0b059861339645f
Git_test <rimmoyee@gmail.com>
2019-08-03 20:20:02 +0900

    전방 패널 조정

--- Assets/02.Scripts/JH/HUDUI/FrontGlassHUD.cs
+++ Assets/02.Scripts/JH/HUDUI/FrontGlassHUD.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 public class FrontGlassHUD : MonoBehaviour
 {
-    public Transform cam;
+
+    public Transform fighterBody;
 
     public RawImage horizontalLine;
     public Transform VerticalLine;
@@ -22,19 +23,19 @@ public class FrontGlassHUD : MonoBehaviour
     void Update()
     {
         //수직 각도 변경값 받기
-        if(cam.localRotation.x > -0.7f && cam.localRotation.x < 0.7f)
+        if(fighterBody.rotation.x > -0.7f && fighterBody.rotation.x < 0.7f)
         {
-            pitch = cam.localRotation.x / 7 * 10;
+            pitch = fighterBody.rotation.x / 7 * 10;
         }
         //print(pitch * 800);
 
         //수직 각도 변경
         VerticalLine.localPosition = new Vector3(0, pitch * 800, 0);
 
-        //print(cam.localRotation.eulerAngles.y);
+        //print(fighterBody.rotation.eulerAngles.y);
 
         //수평 각도 변경값 받기
-        yaw = cam.localRotation.eulerAngles.y / 360;
+        yaw = fighterBody.rotation.eulerAngles.y / 360;
 
         //수평 각도 변경
         horizontalLine.uvRect = new Rect(yaw, 0, 1, 1);
