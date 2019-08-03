6381e313b278cdb323c8127ae844b90ed0900302
Git_test <rimmoyee@gmail.com>
2019-08-03 20:03:11 +0900

    상태표시패널 일부구현

---
+++ Assets/02.Scripts/JH/Instrument panel/StatusIP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIP : MonoBehaviour
+{
+    public AviationManager aviationManager;
+
+    public Text landingGearStatus;
+    public Text afterBurnerStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //부스터 상태 출력
+        switch(aviationManager.booster)
+        {
+            case 0:     //부스터 안씀
+                afterBurnerStatus.text = "OFF";
+                break;
+
+            case 1:     //부스터 씀
+                afterBurnerStatus.text = "ON";
+                break;
+        }
+    }
+}
