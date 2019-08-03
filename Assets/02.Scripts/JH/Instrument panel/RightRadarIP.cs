9a77b390a177bcc9027c533334f5330b660c1379
Git_test <rimmoyee@gmail.com>
2019-08-03 19:54:58 +0900

    레이더 기초구현 완료

--- Assets/02.Scripts/JH/Instrument panel/RightRadarIP.cs
+++ Assets/02.Scripts/JH/Instrument panel/RightRadarIP.cs
@@ -8,17 +8,21 @@ public class RightRadarIP : MonoBehaviour
 
     public Transform radar;
 
+    public Transform raderLine;
 
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         float y = fighterBody.rotation.eulerAngles.y;
+
+        time += Time.deltaTime;
 
         radar.localRotation = Quaternion.Euler( new Vector3(0, 0,
                             360 - ((y + 180) % 360)) );
@@ -28,5 +32,6 @@ public class RightRadarIP : MonoBehaviour
 
         //12시부터 시계방향으로 360 - z 한바퀴
         //print(radar.localRotation.eulerAngles.z);
+        raderLine.localRotation = Quaternion.Euler(new Vector3(0, 0, -(raderLine.localRotation.z + time * 120) % 360));
     }
 }
