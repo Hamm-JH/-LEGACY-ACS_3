fd24b7f438382ff19d38ae4cfaf50381d5528cdf
Git_test <rimmoyee@gmail.com>
2019-08-04 13:17:07 +0900

    FlightHUD 주시방향계, 고도계 구현

---
+++ Assets/02.Scripts/JH/HUDUI/FlightHUD/FlightHUD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlightHUD : MonoBehaviour
+{
+    public Transform fighterBody;
+    public Transform cam;       //HeadingMeter 바꿀 카메라각도
+
+    public RawImage headingMeter;   //보는 방향 알려주는 눈금
+
+    public Transform altiMeter;     //고도계 확인용
+    public Text altiMeterText;      //고도 확인 텍스트
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //headingMeter 출력변환
+        headingMeter.uvRect = new Rect(cam.localRotation.eulerAngles.y / 360, 0, 1, 1);
+
+        altiMeter.localPosition = new Vector3(-52.95f, -1.5f - 91.42f * fighterBody.position.y / 500, 0);
+
+        altiMeterText.text = fighterBody.position.y.ToString("N1");
+    }
+}
