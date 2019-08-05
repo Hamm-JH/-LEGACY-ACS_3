a07b73d22be25939ae83f9f0cc2ca76544263336
Ham-JH <52693438+Ham-JH@users.noreply.github.com>
2019-08-05 19:37:50 +0900

    갱신사항 업로드

---
+++ Assets/02.Scripts/JH/AudioControl/AudioController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [Header("Aviation status")]
+    public AviationManager aviationManager;
+
+    [Header("Audio sources")]
+    public AudioSource lowPass;
+    public AudioSource highPass;
+    public AudioSource highPass2;
+
+    public AudioSource airResistLeft;
+    public AudioSource airRestRight;
+
+    [Header("values")]
+    private float currForce;
+    private float throttle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currForce = aviationManager.thrustControl.currForce;
+        throttle = aviationManager.throttle;
+
+        lowPass.volume = 1 - throttle;
+        highPass.volume = throttle;
+        highPass2.volume = highPass.volume + 0.2f;
+
+        airResistLeft.volume = currForce / 3000;
+        airRestRight.volume = currForce / 3000;
+    }
+}
